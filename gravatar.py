c1ca7a2d409aa83057fd36ff456088611edce500
YasinYAMAN <yasnyaman@gmail.com>
2011-03-27 08:39:41 -0700

    Düzenleme

--- gravatar.py
+++ gravatar.py
@@ -1,7 +1,8 @@
 class Gravatar:
-  root_url = "http://www.gravatar.com/avatar/"
-  def __init__(self, email):
+  root_url = "http://www.gravatar.com/avatar.php"
+  def __init__(self, email, size):
     self.email = email
+    self.size = size
     self.gravatar_id = self.gravatar_id()
     self.gravatar_url = self.gravatar_url()
   def gravatar_id(self):
@@ -9,4 +10,4 @@ class Gravatar:
     return hashlib.md5(self.email.lower()).hexdigest()
   def gravatar_url(self):
     import urllib
-    return "%s%s" % (self.__class__.root_url, self.gravatar_id)
+    return "%s?gravatar_id=%s&size=%s" % (self.__class__.root_url, self.gravatar_id, self.size)
